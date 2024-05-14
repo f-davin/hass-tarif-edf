8be499c77d1254614dafe53b75b021fa3b8717f9
f-davin <8143811+f-davin@users.noreply.github.com>
2024-05-14 23:58:36 +0200

    feat: update const with coefficient for tempo and reformat files

--- custom_components/tarif-edf/sensor.py
+++ custom_components/tarif-edf/sensor.py
@@ -20,41 +20,100 @@ from .const import (
     CONTRACT_TYPE_TEMPO,
 )
 
+
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     coordinator: TarifEdfDataUpdateCoordinator = hass.data[DOMAIN][
         config_entry.entry_id
     ]["coordinator"]
 
     sensors = []
-    if coordinator.data['contract_type'] == CONTRACT_TYPE_BASE:
+    if coordinator.data["contract_type"] == CONTRACT_TYPE_BASE:
         sensors = [
-            TarifEdfSensor(coordinator, 'base_variable_ttc', 'Tarif Base TTC', 'EUR/kWh'),
+            TarifEdfSensor(
+                coordinator, "base_variable_ttc", "Tarif Base TTC", "EUR/kWh"
+            ),
         ]
-    elif coordinator.data['contract_type'] == CONTRACT_TYPE_HPHC:
+    elif coordinator.data["contract_type"] == CONTRACT_TYPE_HPHC:
         sensors = [
-            TarifEdfSensor(coordinator, 'hphc_variable_hc_ttc', 'Tarif Heures creuses TTC', 'EUR/kWh'),
-            TarifEdfSensor(coordinator, 'hphc_variable_hp_ttc', 'Tarif Heures pleines TTC', 'EUR/kWh'),
+            TarifEdfSensor(
+                coordinator,
+                "hphc_variable_hc_ttc",
+                "Tarif Heures creuses TTC",
+                "EUR/kWh",
+            ),
+            TarifEdfSensor(
+                coordinator,
+                "hphc_variable_hp_ttc",
+                "Tarif Heures pleines TTC",
+                "EUR/kWh",
+            ),
         ]
-    elif coordinator.data['contract_type'] == CONTRACT_TYPE_TEMPO:
+    elif coordinator.data["contract_type"] == CONTRACT_TYPE_TEMPO:
         sensors = [
-            TarifEdfSensor(coordinator, 'tempo_couleur', 'Tarif Tempo Couleur'),
-            TarifEdfSensor(coordinator, 'tempo_variable_hc_ttc', 'Tarif Tempo Heures creuses TTC', 'EUR/kWh'),
-            TarifEdfSensor(coordinator, 'tempo_variable_hp_ttc', 'Tarif Tempo Heures pleines TTC', 'EUR/kWh'),
+            TarifEdfSensor(coordinator, "tempo_couleur", "Tarif Tempo Couleur"),
+            TarifEdfSensor(
+                coordinator,
+                "tempo_variable_bleu_hc_ttc",
+                "Tarif Tempo Bleu Heures creuses TTC",
+                "EUR/kWh",
+            ),
+            TarifEdfSensor(
+                coordinator,
+                "tempo_variable_bleu_hp_ttc",
+                "Tarif Tempo Bleu Heures pleines TTC",
+                "EUR/kWh",
+            ),
+            TarifEdfSensor(
+                coordinator,
+                "tempo_variable_blanc_hc_ttc",
+                "Tarif Tempo Blanc Heures creuses TTC",
+                "EUR/kWh",
+            ),
+            TarifEdfSensor(
+                coordinator,
+                "tempo_variable_blanc_hp_ttc",
+                "Tarif Tempo Blanc Heures pleines TTC",
+                "EUR/kWh",
+            ),
+            TarifEdfSensor(
+                coordinator,
+                "tempo_variable_rouge_hc_ttc",
+                "Tarif Tempo Rouge Heures creuses TTC",
+                "EUR/kWh",
+            ),
+            TarifEdfSensor(
+                coordinator,
+                "tempo_variable_rouge_hp_ttc",
+                "Tarif Tempo Rouge Heures pleines TTC",
+                "EUR/kWh",
+            ),
         ]
 
     async_add_entities(sensors, False)
 
+
 class TarifEdfSensor(CoordinatorEntity, SensorEntity):
     """Representation of a Tarif EDF sensor."""
 
-    def __init__(self, coordinator, coordinator_key: str, name: str, unit_of_measurement: str = None) -> None:
+    def __init__(
+        self,
+        coordinator,
+        coordinator_key: str,
+        name: str,
+        unit_of_measurement: str = None,
+    ) -> None:
         """Initialize the Tarif EDF sensor."""
         super().__init__(coordinator)
-        contract_name = str.upper(self.coordinator.data['contract_type']) + " " + self.coordinator.data['contract_power'] + "kVA"
+        contract_name = (
+            str.upper(self.coordinator.data["contract_type"])
+            + " "
+            + self.coordinator.data["contract_power"]
+            + "kVA"
+        )
 
         self._coordinator_key = coordinator_key
         self._name = name
@@ -63,20 +122,18 @@ class TarifEdfSensor(CoordinatorEntity, SensorEntity):
         self._attr_device_info = DeviceInfo(
             name=f"Tarif EDF - {contract_name}",
             entry_type=DeviceEntryType.SERVICE,
-            identifiers={
-                (DOMAIN, f"Tarif EDF - {contract_name}")
-            },
+            identifiers={(DOMAIN, f"Tarif EDF - {contract_name}")},
             manufacturer="Tarif EDF",
             model=contract_name,
         )
-        if (unit_of_measurement is not None):
+        if unit_of_measurement is not None:
             self._attr_unit_of_measurement = unit_of_measurement
 
     @property
     def native_value(self):
         """Return the state of the sensor."""
         if self.coordinator.data[self._coordinator_key] is None:
-            return 'unavailable'
+            return "unavailable"
         else:
             return self.coordinator.data[self._coordinator_key]
 
@@ -84,11 +141,14 @@ class TarifEdfSensor(CoordinatorEntity, SensorEntity):
     def extra_state_attributes(self):
         """Return the state attributes."""
         return {
-            'updated_at': self.coordinator.last_update_success_time,
-            'unit_of_measurement': self._attr_unit_of_measurement,
+            "updated_at": self.coordinator.last_update_success_time,
+            "unit_of_measurement": self._attr_unit_of_measurement,
         }
 
     @property
     def available(self) -> bool:
         """Return if entity is available."""
-        return self.coordinator.last_update_success and self.coordinator.data[self._coordinator_key] is not None
+        return (
+            self.coordinator.last_update_success
+            and self.coordinator.data[self._coordinator_key] is not None
+        )
